f83266090422377be59f8e5e6c0e75c5cf446122
loveisgala <gabriela.gk@hotmail.com>
2022-07-13 15:02:23 -0300

    Create filter bar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
       monsters: [],
-    }
+    };
+
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
       .then((users) =>
@@ -20,12 +22,19 @@ class App extends Component {
           () => {
             console.log(this.state);
           }));
+
   }
 
   render() {
-
+    console.log('render');
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(e) => {
+          const filteredMonsters = this.state.monsters.filter((m) => m.name.toLowerCase().includes(e.target.value.toLowerCase()));
+          return this.setState(() => {
+            return { monsters: filteredMonsters }
+          });
+        }} />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}><h1>{monster.name}</h1></div>
