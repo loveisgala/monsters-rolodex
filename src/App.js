a446ca1dc89e92636012b68a972519560977e3d1
loveisgala <gabriela.gk@hotmail.com>
2022-07-13 16:03:10 -0300

    create searchField state

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
-
-    console.log('constructor');
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((res) => res.json())
       .then((users) =>
@@ -22,20 +20,14 @@ class App extends Component {
           () => {
             console.log(this.state);
           }));
-
   }
 
   render() {
-    console.log('render');
+    const filteredMonsters = this.state.monsters.filter((m) => m.name.toLowerCase().includes(this.state.searchField.toLowerCase()));
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(e) => {
-          const filteredMonsters = this.state.monsters.filter((m) => m.name.toLowerCase().includes(e.target.value.toLowerCase()));
-          return this.setState(() => {
-            return { monsters: filteredMonsters }
-          });
-        }} />
-        {this.state.monsters.map((monster) => {
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(e) => this.setState({ searchField: e.target.value })} />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}><h1>{monster.name}</h1></div>
           );
